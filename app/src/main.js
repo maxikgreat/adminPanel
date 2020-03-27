50ee577b87302eaad0b3a7c301d7dc831493253f
maxikgreat <maximvasylenko228322@gmail.com>
2020-03-27 21:51:14 +0100

    Create simple api with PHP

--- app/src/main.js
+++ app/src/main.js
@@ -1,3 +1,23 @@
 import $ from 'jquery'
 
-$('h1').fadeOut()
+function getPageList(){
+    $('h1').remove();
+    $.get("./api", data => {
+        data.forEach(file => {
+            $('body').append(`<h1>${file}</h1>`)
+        })
+    }, "JSON");
+}
+
+getPageList();
+
+$("button").click(() => {
+    $.post('./api/createNewPage.php', {
+        "name": $('input').val()
+    }, () => {
+        getPageList();
+    })
+        .fail(() => {
+            alert("Page has already exist!")
+        })
+});
